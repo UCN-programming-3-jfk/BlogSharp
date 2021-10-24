7b9dcd37475ae4475a3afe0581dd0409cfdc51b1
Jakob Farian Krarup <kr@rup.dk>
2021-10-24 10:54:42 +0200

    Removed DI configuration of data access from Web API

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -27,10 +27,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSingleton<IAuthorRepository, AuthorRepository>();
-            services.AddSingleton<IBlogPostRepository, BlogPostRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
