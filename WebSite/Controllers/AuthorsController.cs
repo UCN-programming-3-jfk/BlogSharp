4c2a40494e0294e8791213c1ce63db766d1d599d
Jakob Farian Krarup <kr@rup.dk>
2024-10-30 08:43:40 +0100

    refined UI with link animations

--- WebSite/Controllers/AuthorsController.cs
+++ WebSite/Controllers/AuthorsController.cs
@@ -5,132 +5,108 @@ using System.Threading.Tasks;
 using WebApiClient;
 using WebApiClient.DTOs;
 
-namespace WebSite.Controllers
+namespace WebSite.Controllers;
+/// <summary>
+/// This class implements the ASP.NET Core MVC controller which is mapped to the "/Authors" URI.
+/// It provides access to CRUD functionality for the AuthorDTO.
+/// Notice the [Authorize] attribute on the Edit and Delete actions, which demands the user is authenticated
+/// and will redirect to "/accounts/login" if necessary.
+/// </summary>
+
+public class AuthorsController : Controller
 {
+    private IBlogSharpApiClient _client;
+    public AuthorsController(IBlogSharpApiClient client) => _client = client;
 
-    /// <summary>
-    /// This class implements the ASP.NET Core MVC controller which is mapped to the "/Authors" URI.
-    /// It provides access to CRUD functionality for the AuthorDTO.
-    /// Notice the [Authorize] attribute on the Edit and Delete actions, which demands the user is authenticated
-    /// and will redirect to "/accounts/login" if necessary.
-    /// </summary>
-
-    public class AuthorsController : Controller
+    // GET: AuthorController
+    [HttpGet("Authors/{authorId}/BlogPosts")]
+    public async Task<ActionResult> BlogPosts(int authorId)
     {
+        var author = await _client.GetAuthorByIdAsync(authorId);
+        var blogPosts = await _client.GetBlogPostsFromAuthorIdAsync(authorId);
+        var model = new { Author = author, BlogPosts = blogPosts };
+        return View(model);
+    }
 
-        private IBlogSharpApiClient _client;
 
-        public AuthorsController(IBlogSharpApiClient client)
+    // GET: AuthorController
+    public ActionResult Index() => View();
+
+    // GET: AuthorController/Details/5
+    public ActionResult Details(int id) => View();
+
+    // GET: AuthorController/Create
+    public ActionResult Create() => View();
+
+    // POST: AuthorController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Create(AuthorDto author)
+    {
+        ModelState.Remove("NewPassword");
+        if (!ModelState.IsValid)
         {
-            _client = client;
+            ViewBag.ErrorMessage = "User not created - error in submitted data!";
         }
-
-
-        // GET: AuthorController
-        [HttpGet("Authors/{authorId}/BlogPosts")]
-        public async Task<ActionResult> BlogPosts(int authorId)
-        {
-            var author = await _client.GetAuthorByIdAsync(authorId);
-            var blogPosts = await _client.GetBlogPostsFromAuthorIdAsync(authorId);
-            var model = new { Author = author, BlogPosts = blogPosts };
-            return View(model);
-        }
-
-
-            // GET: AuthorController
-            public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: AuthorController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: AuthorController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: AuthorController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(AuthorDto author)
-        {
-            ModelState.Remove("NewPassword");
-            if (!ModelState.IsValid)
-            {
-                ViewBag.ErrorMessage = "User not created - error in submitted data!";
-            }
-            else
-            {
-                try
-                {
-                    if (await _client.CreateAuthorAsync(author) > 0)
-                    {
-                        TempData["Message"] = $"User {author.Email} created!";
-                        return RedirectToAction(nameof(Index), "Home");
-                    }
-                    else
-                    {
-                        ViewBag.ErrorMessage = "User not created!";
-                    }
-                }
-                catch (Exception ex)
-                {
-                    ViewBag.ErrorMessage = ex.Message;
-                }
-            }
-            return View();
-        }
-
-        // GET: AuthorController/Edit/5
-        [Authorize(Roles = "Author")]
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: AuthorController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Author")]
-        public ActionResult Edit(int id, AuthorDto author)
+        else
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (await _client.CreateAuthorAsync(author) > 0)
+                {
+                    TempData["Message"] = $"User {author.Email} created!";
+                    return RedirectToAction(nameof(Index), "Home");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "User not created!";
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.ErrorMessage = ex.Message;
             }
         }
+        return View();
+    }
 
-        // GET: AuthorController/Delete/5
-        [Authorize(Roles = "Author")]
-        public ActionResult Delete(int id)
+    // GET: AuthorController/Edit/5
+    [Authorize(Roles = "Author")]
+    public ActionResult Edit(int id) => View();
+
+    // POST: AuthorController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize(Roles = "Author")]
+    public ActionResult Edit(int id, AuthorDto author)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
         {
             return View();
         }
+    }
 
-        // POST: AuthorController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Author")]
-        public ActionResult Delete(int id, AuthorDto author)
+    // GET: AuthorController/Delete/5
+    [Authorize(Roles = "Author")]
+    public ActionResult Delete(int id) => View();
+
+    // POST: AuthorController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize(Roles = "Author")]
+    public ActionResult Delete(int id, AuthorDto author)
+    {
+        try
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
         }
     }
 }
